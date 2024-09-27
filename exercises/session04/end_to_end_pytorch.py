867c93fa7f9940bae716a580c3c6f2e8c0f7b128
Diogo Rodrigues <diogo.gorodrigues@gmail.com>
2024-09-27 14:50:54 +0100

    Resolving exercises with Pytorch

--- exercises/session04/end_to_end_pytorch.py
+++
@@ -1,8 +0,0 @@
-# Implement the end-to-end training pipeline using PyTorch
-
-# 1. Load the data
-# 2. Define the model
-# 3. Define the loss function
-# 4. Define the optimizer
-# 5. Train the model
-# 6. Make predictions
