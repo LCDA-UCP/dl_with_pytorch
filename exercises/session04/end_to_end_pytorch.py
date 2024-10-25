fc997af4841e8ce5dc4bf59ba537a350e25427be
tiago <tiagosilvavieira06@gmail.com>
2024-10-25 11:14:54 +0100

    Tiago Vieira exercicios

--- exercises/session04/end_to_end_pytorch.py
+++ exercises/session04/end_to_end_pytorch.py
@@ -1,6 +1,11 @@
-# Implement the end-to-end training pipeline using PyTorch
+import torch
+import pandas as pd
+print(torch.cuda.is_available())
+
 
 # 1. Load the data
+data = pd.read_csv('/Users/tiago/dl_with_pytorch/data/diabetes.csv')
+print(data)
 # 2. Define the model
 # 3. Define the loss function
 # 4. Define the optimizer
